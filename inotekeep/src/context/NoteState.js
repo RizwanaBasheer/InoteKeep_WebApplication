44c58f3591cd6bed1df331d2a19287a06e751ce3
RizwanaBasheer <20031946@mydbs.ie>
2024-07-01 22:42:26 +0100

    error

--- inotekeep/src/context/NoteState.js
+++ inotekeep/src/context/NoteState.js
@@ -62,6 +62,46 @@ const NoteState = props => {
   };
 
 
+  // Edit a Note
+  const editNote = async (id, title, description, tag) => {
+    // API calls
+    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": localStorage.getItem("token"),
+      },
+      body: JSON.stringify({title, description, tag}),
+    });
+    const json = await response.json();
+    console.log(json);
+
+    let newNotes = JSON.parse(JSON.stringify(notes));
+    // Logic to edit in client
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
+
+      // I want only that note whose id matches with the given id so that I can update that particular note
+      if (element._id=== id) {
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
+      }
+    }
+    setNotes(newNotes);
+  };
+
+  // Use a Provider to pass the current theme to the tree below.
+  // Any component can read it, no matter how deep it is.
+  // In this example, we're passing "notes, addNote, deleteNote, editNote, getNotes" as the current values
+  return (
+    <NoteContext.Provider
+      value={{notes, addNote, deleteNote, editNote, getNotes}}
+    >
+      {props.children}
+    </NoteContext.Provider>
+  );
 };
 
 export default NoteState;
