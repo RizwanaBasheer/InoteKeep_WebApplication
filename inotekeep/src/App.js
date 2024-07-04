94e8208b88a25f8ce2ff02d33d56b5aae51ba2c6
RizwanaBasheer <20031946@mydbs.ie>
2024-07-04 15:57:02 +0100

    ADDING THE CSS AND JS

--- inotekeep/src/App.js
+++ inotekeep/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import './App.css'
+const App = () => {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
 
-function App() {
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  //in the routes, I am passing showAlert as the prop so that I can use alert functionality easily
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert alert={alert} />
+          <Routes>
+            {localStorage.getItem("token") ? <Route exact path="/" element={<Home showAlert={showAlert} />} /> : <Route exact path="/" element={<Login showAlert={showAlert} />} />}
+            <Route exact path="/" element={<Home showAlert={showAlert} />} />
+            <Route exact path="/" element={<Home showAlert={showAlert} />} />
+            <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+            <Route exact path="/signup" element={<Signup showAlert={showAlert} />} />
+          </Routes>
+        </Router>
+      </NoteState>
+    </>
   );
-}
+};
 
 export default App;
