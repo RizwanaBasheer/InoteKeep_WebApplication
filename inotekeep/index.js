c10bc16ca09bc8a5445215f42443eb9b65e50278
RizwanaBasheer <20031946@mydbs.ie>
2024-07-06 21:43:39 +0100

    changed 1

--- inotekeep/index.js
+++ inotekeep/index.js
@@ -9,11 +9,16 @@ const port = 5000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('./client/build'));
+
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+app.get("*", (req, res) => { //our GET route needs to point to the index.html in our build
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`)
